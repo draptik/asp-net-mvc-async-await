1026551b46cfb646dcbc4b9abc4dc3a802cc523d
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-07-16 19:33:44 +0200

    clean install

---
+++ DemoWebApp/DemoWebApp.Web/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace DemoWebApp.Web.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        private List<CustomerVm> GetCustomers()
+        {
+            var result = new List<CustomerVm>();
+            for (int i = 0; i < 10; i++)
+            {
+                result.Add(new CustomerVm {FirstName = "foo " + i.ToString()});
+            }
+            return result;
+        }
+    }
+
+    public class CustomerVm
+    {
+        public string FirstName { get; set; }
+    }
+}
---
+++ DemoWebApp/DemoWebApp.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoWebApp.Web.MvcApplication" Language="C#" %>
